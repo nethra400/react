62285fa3ec2c8cd09db9775cfb98b753efafb402
nethra400 <61528928+nethra400@users.noreply.github.com>
2021-05-26 10:43:51 +0530

    Add files via upload

---
+++ fizz.js
@@ -0,0 +1,8 @@
+let i=15;
+if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+} else if (i % 3 === 0) {
+    console.log("Fizz");
+} else if (i % 5 === 0) {
+    console.log("Buzz");
+}
